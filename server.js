c4e5e3dac7311285c0caa86d3e877cccd577f097
EJ-C <10ngweekendofficial@gmail.com>
2020-01-02 03:03:49 -0500

    Updating database

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-polished-58583',
-    user : '',
-    password : '',
-    database : 'facerecog'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
+    
   }
 });
 
