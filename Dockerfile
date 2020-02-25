ab2e63ec312b37f0e9fbaf022310fc8d5809f62a
EJ-C <10ngweekendofficial@gmail.com>
2020-02-24 20:50:26 -0500

    o

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,16 @@
 FROM node:stretch
 
+RUN mkdir -p /usr/src/smart-brain-api
 WORKDIR /usr/src/smart-brain-api
 
-COPY ./ ./
-
+COPY package.json /usr/src/smart-brain-api
 RUN npm install
 
-CMD ["/bin/bash"]
+# Bundle app source
+COPY . /usr/src/smart-brain-api
+
+# Build arguments
+ARG NODE_VERSION=stretch
+
+# Environment
+ENV NODE_VERSION $NODE_VERSION
--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+const morgan = require('morgan');
 
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-
+const auth = require('./controllers/authorization');
 
 const db = knex({
   client: 'pg',
@@ -25,17 +26,17 @@ const db = knex({
 // });
 
 const app = express();
-
-
 app.use(cors())
 app.use(bodyParser.json());
+app.use(morgan('combine'));
 
 app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => {profile.handleProfileUpdate(req, res, db)})
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, ()=> {
   console.log(`app is running on port ${process.env.PORT}`);
